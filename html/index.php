085e9647b30967b050b43de3be1054f69e6a37e2
akrherz <akrherz@iastate.edu>
2013-03-05 13:42:24 -0600

    port over the iembot monitor app, gonna run from here rather than IEM

--- html/index.php
+++ html/index.php
@@ -1,10 +1,18 @@
 <html>
 <head>
- <title>weather.im</title>
+ <title>weather.im, powered by Iowa Environmental Mesonet</title>
 </head>
 <body>
 
-<h3>Hello World....</h3>
+<h3>Weather.IM</h3>
+
+<p>This website is a testbed of weather dissemination technologies powered
+by the Iowa Environmental Mesonet of Iowa State University.  These products
+and services are provided without warranty.
+
+<ul>
+ <li><a href="/iembot/">IEMBot Monitor</a></li>
+</ul>
 
 </body>
 </html>
