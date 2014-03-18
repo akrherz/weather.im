f59693f7316e6c66d84666ad036954d4a982e592
akrherz <akrherz@iastate.edu>
2014-03-18 15:36:42 -0500

    WIP allow Live client to run here

---
+++ include/props.php
@@ -0,0 +1,12 @@
+<?php 
+// Fake values
+$config = Array(
+	"punjab_route" => "xmpp:nwschat-openfire:5222",
+	"bosh_service" => "/bosh/",
+	"xmpp_domain" => "laptop.local",
+	"xmpp_mucservice" => "conference.laptop.local",
+	"nwschatadmin" => "akrherz@localhost",	
+	"live_appname" => "NWSChat Live",
+);
+
+?>
