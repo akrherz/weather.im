f4e94380f0cf94efe843752f950c02c5ebff839a
akrherz <akrherz@iastate.edu>
2014-06-03 13:50:46 -0500

    add placeholder

---
+++ html/p.php
@@ -0,0 +1,15 @@
+<?php
+
+$id = isset($_GET['id']) ? intval($_GET['id']) : "";
+$pid = isset($_GET['pid']) ? substr($_GET['pid'],0,32) : "";
+if ($id == "" && $pid == "") die();
+
+if ($id != "") {
+  die("Sorry, this interface is no longer available!  Please contact us if you need it back!");
+} else {
+  // 201212100547-KTOP-FXUS63-AFDTOP
+  header(sprintf("Location: http://mesonet.agron.iastate.edu/wx/afos/p.php?pil=%s&e=%s", 
+  	substr($pid,25,6), substr($pid,0,12)));
+  exit;
+}
+?>
