0b8183f8fc87058545e75028413b8cc5b54a8e25
akrherz <akrherz@iastate.edu>
2019-12-10 22:43:02 -0600

    add missing default props

--- include/props.php
+++ include/props.php
@@ -6,7 +6,10 @@ $config = Array(
 	"xmpp_domain" => "laptop.local",
 	"xmpp_mucservice" => "conference.laptop.local",
 	"nwschatadmin" => "akrherz@localhost",	
-	"live_appname" => "NWSChat Live",
+    "live_appname" => "NWSChat Live",
+	"live_login_opt_user" => "true",
+	"live_login_opt_anonymous" => "true",
+	"live_login_opt_register" => "true",
 );
 
 ?>
