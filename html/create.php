306811f6114ec04530f63dfa74d90a6b42bbdcf4
akrherz <akrherz@iastate.edu>
2023-11-11 12:46:40 -0600

    chore: apply formatting

--- html/create.php
+++ html/create.php
@@ -1,44 +1,47 @@
-<?php 
+<?php
 // Registration page for generating Weather.IM Accounts
 require_once "../config/settings.inc.php";
 require_once "../include/myview.php";
 
 $content = null;
-if (isset($_POST["agree"]) && $_POST["botq"] == 'iowa'){
-	// Start of new account logic!
-	$p1 = isset($_POST["p1"]) ? $_POST["p1"]: null;
-	$p2 = isset($_POST["p2"]) ? $_POST["p2"]: null;
-	$user = isset($_POST["username"]) ? $_POST["username"]: null;
-	$email = isset($_POST["email"]) ? $_POST["email"]: null;
-    if ($p1 == null || $p2 == null || $user == null || $email == null ||
-        $p1 != $p2){
-		// NOOP
-	} else {
-	// Construct the dict payload
-	$payload = Array(
-			"username" => $user,
-			"password" => $p1,
-			"email" => $email
-		);
+if (isset($_POST["agree"]) && $_POST["botq"] == 'iowa') {
+    // Start of new account logic!
+    $p1 = isset($_POST["p1"]) ? $_POST["p1"] : null;
+    $p2 = isset($_POST["p2"]) ? $_POST["p2"] : null;
+    $user = isset($_POST["username"]) ? $_POST["username"] : null;
+    $email = isset($_POST["email"]) ? $_POST["email"] : null;
+    if (
+        $p1 == null || $p2 == null || $user == null || $email == null ||
+        $p1 != $p2
+    ) {
+        // NOOP
+    } else {
+        // Construct the dict payload
+        $payload = array(
+            "username" => $user,
+            "password" => $p1,
+            "email" => $email
+        );
 
 
-		$ch = curl_init("http://iem-openfire:9090/plugins/restapi/v1/users");
-	$pay = json_encode($payload);
-	curl_setopt($ch, CURLOPT_POST, TRUE);
-	curl_setopt($ch, CURLOPT_POSTFIELDS, $pay);
-	curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json',
-		sprintf("Authorization: %s", $config["openfire_userservice_secret"])));
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	$result = curl_exec($ch);
-	curl_close($ch);
-		if ($result === FALSE){
-			$content = sprintf("User Creation failed!");
-		}
-		else {
-	$content = <<<EOF
-	<p>Congratulations, your account "$user@weather.im" has been created.
-		You can now use a XMPP client to connect to our server or directly
-	use the <a href="/live/">Live Application</a></p>
+        $ch = curl_init("http://iem-openfire:9090/plugins/restapi/v1/users");
+        $pay = json_encode($payload);
+        curl_setopt($ch, CURLOPT_POST, TRUE);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $pay);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+            'Content-Type:application/json',
+            sprintf("Authorization: %s", $config["openfire_userservice_secret"])
+        ));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $result = curl_exec($ch);
+        curl_close($ch);
+        if ($result === FALSE) {
+            $content = sprintf("User Creation failed!");
+        } else {
+            $content = <<<EOF
+    <p>Congratulations, your account "$user@weather.im" has been created.
+        You can now use a XMPP client to connect to our server or directly
+    use the <a href="/live/">Live Application</a></p>
 EOF;
         }
     }
@@ -46,8 +49,8 @@ EOF;
 
 $t = new MyView();
 $t->title = "Account Registration";
-if ($content == null){
-$t->content = <<<EOF
+if ($content == null) {
+    $t->content = <<<EOF
 <h3>Weather.IM XMPP/Jabber Account Registration</h3>
 
 <p>Due to spammers and bots, we need to have you register for an account via
@@ -59,8 +62,8 @@ like that.</p>
   <div class="checkbox">
     <label>
       <input type="checkbox" name="agree"> I am at least 13 years of age and
-	agree that my usage is without expressed or implied warranty by the
-	Weather.IM Project
+    agree that my usage is without expressed or implied warranty by the
+    Weather.IM Project
     </label>
   </div>
  
@@ -85,14 +88,12 @@ like that.</p>
     <input name="botq" type="text" class="form-control" id="botq" placeholder="type iowa">
   </div>
   <button type="submit" class="btn btn-default">Submit</button>
-		
-		
+        
+        
 </form>
 
 EOF;
 } else {
-	$t->content = $content;
+    $t->content = $content;
 }
 $t->render("single.phtml");
-
-?>
