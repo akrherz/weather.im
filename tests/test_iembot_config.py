2154b6b000ba2bafb590dead485cacbcd874f534
akrherz <akrherz@iastate.edu>
2026-02-21 16:18:40 -0600

    🎨 Iterate on coverage needs

--- tests/test_iembot_config.py
+++ tests/test_iembot_config.py
@@ -4,6 +4,14 @@ from weather_im.iembot_config import application
 from werkzeug.test import Client
 
 
+def test_create_webhook():
+    """Test no user."""
+    c = Client(application)
+    resp = c.post("/", data={"action": "create_webhook"})
+    assert resp.status_code == 200
+    assert b"Webhook URL and label are required" in resp.data
+
+
 def test_simple():
     """Test a simple call."""
     c = Client(application)
