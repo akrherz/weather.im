d9dbf76c8cb1500a5b274a0fb11a781d5b43f59c
akrherz <akrherz@iastate.edu>
2026-08-14 23:55:24 -0500

    📝 Updates per migration

--- html/index.php
+++ html/index.php
@@ -22,6 +22,10 @@ and services are provided without warranty.<p>
 <h4>News and Notes</h4>
 
 <ul>
+<li>
+    <strong>14 August 2026</strong>: The service was migrated to a cloud server
+    to improve stability.
+</li>
 <li><strong>On 1 August 2023</strong>, the National Weather Service (NWS) migrated from NWSChat v1.0
 to a version based on <a href="https://www.noaa.gov/NWSChat">Slack</a>.  This change
 does not impact the tools found here.</li>
